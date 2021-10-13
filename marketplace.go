1b4e2a7e61a57cfc07d45aa04fb04bad32dbb160
hodaaghaei <33236617+hodaaghaei@users.noreply.github.com>
2021-10-13 19:38:51 -0400

    Update marketplace.go

--- marketplace.go
+++ marketplace.go
@@ -20,9 +20,9 @@ import (
 	"encoding/xml"
 	"errors"
 
-	"github.com/OpenNebula/one/src/oca/go/src/goca/parameters"
-	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/marketplace"
-	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
+	"github.com/hodaaghaei/goca/parameters"
+	"github.com/hodaaghaei/goca/schemas/marketplace"
+	"github.com/hodaaghaei/goca/schemas/shared"
 )
 
 // MarketPlacesController is a controller for a pool of MarketPlaces
