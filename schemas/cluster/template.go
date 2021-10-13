99d1babd579256ba02866a678f220607185306ba
hodaaghaei <33236617+hodaaghaei@users.noreply.github.com>
2021-10-13 18:43:45 -0400

    Update template.go

--- schemas/cluster/template.go
+++ schemas/cluster/template.go
@@ -17,8 +17,8 @@
 package cluster
 
 import (
-	dyn "github.com/hodaaghaei/onego/dynamic"
-	"github.com/hodaaghaei/onego/schemas/cluster/keys"
+	dyn "github.com/hodaaghaei/goca/dynamic"
+	"github.com/hodaaghaei/goca/schemas/cluster/keys"
 )
 
 // Template is a cluster template
