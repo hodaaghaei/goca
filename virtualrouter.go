191d2145fe711e7eb16fa437e9197576f4e7995f
hodaaghaei <33236617+hodaaghaei@users.noreply.github.com>
2021-10-13 20:03:19 -0400

    Update virtualrouter.go

--- virtualrouter.go
+++ virtualrouter.go
@@ -20,9 +20,9 @@ import (
 	"encoding/xml"
 	"errors"
 
-	"github.com/OpenNebula/one/src/oca/go/src/goca/parameters"
-	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
-	vr "github.com/OpenNebula/one/src/oca/go/src/goca/schemas/virtualrouter"
+	"github.com/hodaaghaei/goca/parameters"
+	"github.com/hodaaghaei/goca/schemas/shared"
+	vr "github.com/hodaaghaei/goca/schemas/virtualrouter"
 )
 
 // VirtualRoutersController is a controller for a pool of VirtualRouters
